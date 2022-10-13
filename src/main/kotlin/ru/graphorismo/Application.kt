9efd85d1b53f4587398ea4535e9e3eced54c66df
graphorismo <graphorismo@gmail.com>
2022-10-13 11:13:41 +0300

    Init ktor project.

---
+++ src/main/kotlin/ru/graphorismo/Application.kt
@@ -0,0 +1,14 @@
+package ru.graphorismo
+
+import io.ktor.server.application.*
+import ru.graphorismo.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureRouting()
+}
